b99c8cecbf8535836f80a90ba43096c9bf31258f
Angela <tiangela1@gmail.com>
2021-01-09 14:32:59 -0800

    Setup basic template, with functionality for basic fetching of resopnsibilities using jQuery.

---
+++ contentScript.js
@@ -0,0 +1,19 @@
+function fetchResponsibilities() {
+    var temp = $(".job-description .jd-info ul li").get();
+    var res = []
+
+    for (idx = 0, len = temp.length; idx < len; idx++) {
+        str = temp[idx].innerHTML;
+        res.push(str);
+    }
+
+    return res;
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.message == "fetchResponsibilities") {
+        sendResponse({responsibilities: fetchResponsibilities()});
+      }
+    }
+  );
---
+++ popup.js
@@ -0,0 +1,21 @@
+let submit = document.getElementById('submit');
+let ul = document.getElementById('requirements');
+let requirementsTitle = document.getElementById('requirements-title');
+var li = document.createElement("li");
+
+submit.onclick = connect;
+
+function connect() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {message: "fetchResponsibilities"}, function(response) {
+        console.log(response.responsibilities);
+        requirementsTitle.innerHTML = "Reponsibilities";
+
+        for(idx = 0; idx < response.responsibilities.length; idx++) {
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode(response.responsibilities[idx]));
+          ul.appendChild(li);
+        }
+      });
+    });
+}
