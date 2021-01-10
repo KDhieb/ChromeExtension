140ea1c5d735de1508be571c2fd9e5bc3dd81970
Angela <tiangela1@gmail.com>
2021-01-09 20:58:55 -0800

    Included basic Emsi functionality.

--- contentScript.js
+++ contentScript.js
@@ -10,10 +10,55 @@ function fetchResponsibilities() {
     return res;
 }
 
+let auth_endpoint = "https://auth.emsicloud.com/connect/token"
+
+let client_id = "ihil071p6tbsgy18";
+let secret = "uUzekrLN";
+let scope = "emsi_open";
+
+let data = {
+    client_id: client_id,
+    client_secret: secret,
+    grant_type: "client_credentials",
+    scope: scope
+}
+
+function sendToEmsi(sendResponse) {
+    let token = "";
+    $.post({
+        type: "POST",
+        url: auth_endpoint,
+        data: data,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Content-Type", "application/x-www-form-urlencoded");
+        },
+        success: function(data){
+            token = data.access_token;
+            
+            let text = {
+                "text": "... Great candidates also have\n\n Experience with a particular JS MV* framework (we happen to use React)\n Experience working with databases\n Experience with AWS\n Familiarity with microservice architecture\n Familiarity with modern CSS practices, e.g. LESS, SASS, CSS-in-JS ..."
+            }
+
+            $.post({
+                type: "POST",
+                url: "https://cors-anywhere.herokuapp.com/https://emsiservices.com/skills/versions/latest/extract/trace",
+                data: JSON.stringify(text),
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader ("Authorization", `Bearer ${token}`);
+                    xhr.setRequestHeader ("Content-Type", "application/json");
+                },
+                success: function(data){
+                    sendResponse({responsibilities: fetchResponsibilities(), skills: data});
+                }
+            })
+        }
+    })
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.message == "fetchResponsibilities") {
-        sendResponse({responsibilities: fetchResponsibilities()});
+        sendToEmsi(sendResponse);
       }
     }
   );
--- popup.js
+++ popup.js
@@ -9,7 +9,8 @@ function connect() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {message: "fetchResponsibilities"}, function(response) {
         console.log(response.responsibilities);
-        requirementsTitle.innerHTML = "Reponsibilities";
+        console.log(response.skills);
+        requirementsTitle.innerHTML = "Responsibilities";
 
         for(idx = 0; idx < response.responsibilities.length; idx++) {
           var li = document.createElement("li");
